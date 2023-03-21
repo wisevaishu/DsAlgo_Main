fcf1c699930f45295a9cc3375f8f6c45b394623a
wisevaishu <vpm.win@gmail.com>
2023-03-21 00:33:43 -0700

    final commit for allure results and with cross browser code

--- src/test/java/testRunner/testRun.java
+++ src/test/java/testRunner/testRun.java
@@ -11,10 +11,10 @@ public class testRun
 	(		
 		features = 
 			{
-				//"src/test/resources/Features/TestCasesAll.feature",
+				"src/test/resources/Features/TestCasesAll.feature",
 				"src/test/resources/Features/login.feature",
-				//"src/test/resources/Features/registerExcel.feature",
-				//"src/test/resources/Features/Home.feature"	
+				"src/test/resources/Features/registerExcel.feature",
+				"src/test/resources/Features/Home.feature"	
 			},
 		plugin = {"pretty", "html:target/SystemTestReports/index.html",  
 				  "junit:target/SystemTestReports/junit/report.xml",
